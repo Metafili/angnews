1103236228a4ee7b8f3223e2c289b5e220096ea9
deni <metafili@gmail.com>
2015-11-27 23:48:07 +0000

    3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,20 +20,37 @@ var app = angular.module('angNewsApp', [
     'ngTouch',
     'firebase'
   ])
-.constant('FIREBASE_URL', 'https://PUT-YOUR-FIREBASE-URL-HERE.firebaseio.com/');
+.constant('FIREBASE_URL', 'https://torrid-inferno-6468.firebaseio.com/');
 
 app.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-    templateUrl: 'views/posts.html',
-    controller: 'PostsCtrl'
-  })
-  .when('/posts/:postId', {
-    templateUrl: 'views/showpost.html',
-    controller: 'PostViewCtrl'
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
-      
+	.when('/', {
+	  templateUrl: 'views/posts.html',
+	  controller: 'PostsCtrl'
+	})
+    .when('/posts/:postId', {
+      templateUrl: 'views/showpost.html',
+      controller: 'PostViewCtrl'
+    })
+	.when('/register', {
+	  templateUrl: 'views/register.html',
+	  controller: 'AuthCtrl',
+	  resolve: {
+		user: function(Auth) {
+		  return Auth.resolveUser();
+		}
+	  }
+	})
+	.when('/login', {
+  templateUrl: 'views/login.html',
+  controller: 'AuthCtrl',
+  resolve: {
+    user: function(Auth) {
+      return Auth.resolveUser();
+    }
+  }
+})
+	.otherwise({
+	redirectTo: '/'
+	});
   });
--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,21 +1,17 @@
 'use strict';
 
-app.controller('PostsCtrl', function ($scope, $location, Post) {
+app.controller('NavCtrl', function ($scope, $location, Post, Auth) {
+  $scope.signedIn = Auth.signedIn;
+$scope.logout = Auth.logout;
 
-  $scope.posts = Post.all;
 
-  $scope.post = {url: 'http://', 'title': ''};
+  $scope.post = {url: 'http://', title: ''};
 
   $scope.submitPost = function () {
     Post.create($scope.post).then(function (ref) {
       $location.path('/posts/' + ref.name());
       $scope.post = {url: 'http://', title: ''};
     });
-  };
-
-
-  $scope.deletePost = function (post) {
-    Post.delete(post);
   };
 
 });
